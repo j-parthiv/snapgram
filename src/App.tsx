a52564ad46abeae4d7569438c7cdeb6861715f3e
j-parthiv <parthivjani9@gmail.com>
2024-01-11 19:28:47 +0100

    feat: add initial routing and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import "./globals.css";
-
+import { Route, Routes } from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Snapgram!
-    </h1>
+    <main className='flex h-screen'>
+     <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+        <Route path='/sign-in' element={<SigninForm />}/>
+        <Route path='/sign-up' element={<SignupForm />}/>
+        </Route>
+        {/* Private Routes */}
+        <Route element={<RootLayout/>}>
+         <Route index element={<Home />}/>
+        </Route>
+     </Routes>
+    </main>
   )
 }
 
