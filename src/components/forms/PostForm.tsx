e42bb43b269787961a9414c2ce898873a9f95933
j-parthiv <parthivjani9@gmail.com>
2024-01-20 17:28:50 +0100

    feat: add update post functionality

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -16,22 +16,27 @@ import { Textarea } from "../ui/textarea";
 import FileUploader from "../shared/FileUploader";
 import { PostValidation } from "@/lib/validation";
 import { Models } from "appwrite";
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations";
+import {
+  useCreatePost,
+  useUpdatePost,
+} from "@/lib/react-query/queriesAndMutations";
 import { AArrowUpIcon } from "lucide-react";
 import { useUserContext } from "@/context/AuthContext";
 import { toast, useToast } from "../ui/use-toast";
 import { useNavigate } from "react-router-dom";
 
-
 type PostFormProps = {
-    post?: Models.Document;
-    action: 'Create' | 'Update';
-}
+  post?: Models.Document;
+  action: "Create" | "Update";
+};
 const PostForm = ({ post, action }: PostFormProps) => {
-    const { mutateAsync: createPost, isPending: isLoading } = useCreatePost();
-    const {user} = useUserContext();
-    const {toast} = useToast();
-    const navigate = useNavigate();
+  const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+  const { mutateAsync: updatePost, isPending: isLoadingUpdate } =
+    useUpdatePost();
+
+  const { user } = useUserContext();
+  const { toast } = useToast();
+  const navigate = useNavigate();
   // 1. Define your form.
   const form = useForm<z.infer<typeof PostValidation>>({
     resolver: zodResolver(PostValidation),
@@ -45,17 +50,32 @@ const PostForm = ({ post, action }: PostFormProps) => {
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof PostValidation>) {
-   const newPost = await createPost({
-    ...values,
-    userId: user.id,
-   })
+    if (post && action === "Update") {
+      const updatedPost = await updatePost({
+        ...values,
+        postId: post.$id,
+        imageId: post.$imageId,
+        imageUrl: post.$imageUrl,
+      });
 
-   if(!newPost){
-    toast({title: "Please try again"})
-   }
+      if (!updatedPost) {
+        toast({ title: "Please try again!" });
+      }
+      
+      return navigate(`/posts/${post.$id}`);
+    } else {
+      const newPost = await createPost({
+        ...values,
+        userId: user.id,
+      });
 
-   navigate("/");
-    console.log(values);
+      if (!newPost) {
+        toast({ title: "Please try again" });
+      }
+
+      navigate("/");
+      console.log(values);
+    }
   }
 
   return (
@@ -139,8 +159,10 @@ const PostForm = ({ post, action }: PostFormProps) => {
           <Button
             type="submit"
             className="shad-button_primary whitespace-nowrap"
+            disabled={isLoadingUpdate || isLoadingCreate}
           >
-            Submit
+            {isLoadingCreate || isLoadingUpdate && 'Loading...'}
+            {action} Post
           </Button>
         </div>
       </form>
