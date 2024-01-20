667272125658329b33edf4cff9849f32ac5f1b1c
j-parthiv <parthivjani9@gmail.com>
2024-01-20 21:10:22 +0100

    feat: add a show search results component

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,26 @@
+import { Models } from "appwrite";
+import React from "react";
+import Loader from "./Loader";
+import GridPostList from "./GridPostList";
+
+type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+};
+
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts,
+}: SearchResultProps) => {
+  if (isSearchFetching) {
+    return <Loader />;
+  } else if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  } else {
+    return (
+      <p className="text-light-4 mt-10 text-center w-full">No Results Found</p>
+    );
+  }
+};
+
+export default SearchResults;
