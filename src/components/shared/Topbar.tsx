9995dfd0bdf55da7980e3a6a734186af44205254
j-parthiv <parthivjani9@gmail.com>
2024-01-20 17:29:59 +0100

    fix: fix image path error in topbar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,24 +1,26 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
-import { useNavigate } from "react-router-dom";
 import { useUserContext } from "@/context/AuthContext";
+
 const Topbar = () => {
   const navigate = useNavigate();
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+
   useEffect(() => {
     if (isSuccess) {
       navigate(0);
     }
   }, [isSuccess]);
+
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
           <img
-            src="./assets/images/logo.svg"
+            src="/assets/images/logo.svg"
             alt="logo"
             width={130}
             height={325}
@@ -32,7 +34,7 @@ const Topbar = () => {
           >
             <img src="./assets/icons/logout.svg" alt="logout" />
           </Button>
-          <Link to={`/profile/${user.id}`}>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
             <img
               src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
               alt="profile"
