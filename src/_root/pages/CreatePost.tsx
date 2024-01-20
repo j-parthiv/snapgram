d33f8a59d0260d247725d3be0e48ddd632baf282
j-parthiv <parthivjani9@gmail.com>
2024-01-20 13:35:33 +0100

    feat: ceate edit post page

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,4 +1,5 @@
 import PostForm from "@/components/forms/PostForm";
+
 const CreatePost = () => {
   return (
     <div className="flex flex-1">
@@ -12,7 +13,7 @@ const CreatePost = () => {
           />
           <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
         </div>
-        <PostForm />
+        <PostForm action="Create" />
       </div>
     </div>
   );
--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -25,8 +25,9 @@ import { useNavigate } from "react-router-dom";
 
 type PostFormProps = {
     post?: Models.Document;
+    action: 'Create' | 'Update';
 }
-const PostForm = ({ post }: PostFormProps) => {
+const PostForm = ({ post, action }: PostFormProps) => {
     const { mutateAsync: createPost, isPending: isLoading } = useCreatePost();
     const {user} = useUserContext();
     const {toast} = useToast();
